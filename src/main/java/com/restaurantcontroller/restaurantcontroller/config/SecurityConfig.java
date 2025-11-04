84326657c9f87512a1a49111b493a37e9ed40bbf
MaykPlatero <maykp.platero1910@gmail.com>
2025-11-03 22:48:21 -0300

    Adicionando requestMatcher(HttpMethod)

--- src/main/java/com/restaurantcontroller/restaurantcontroller/config/SecurityConfig.java
+++ src/main/java/com/restaurantcontroller/restaurantcontroller/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.restaurantcontroller.restaurantcontroller.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,6 +44,7 @@ public class SecurityConfig {
                     "/swagger-resources/**",
                     "/webjars/**"
                 ).permitAll()
+                    .requestMatchers(HttpMethod.POST, "/users").permitAll()
                 .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults())
